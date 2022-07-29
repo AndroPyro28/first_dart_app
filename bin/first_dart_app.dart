28b5846b891f89750640d7568501340e70cd85f3
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-29 06:07:05 -0400

    Classess

--- bin/first_dart_app.dart
+++ bin/first_dart_app.dart
@@ -1,15 +1,27 @@
 import 'package:first_dart_app/first_dart_app.dart' as first_dart_app;
 
 void main(List<String> arguments) {
-  User newUser = User(name: "andro", photoUrl: "some.png");
+  User newUser1 = User(name: "andro eugenio", photoUrl: "some.png");
+  print(newUser1.hasLongName());
 }
 
 class User {
-  String name;
-  String photoUrl;
-  User ({
+  final String name;
+  final String photoUrl;
+
+  const User ({
     required this.name,
     required this.photoUrl
-  })
+  });
+
+  bool hasLongName () {
+
+    return name.length > 10;
+  }
   
+  static void myMethod() {
+
+  }
+
+  static const minNameLength = 3;
 }
