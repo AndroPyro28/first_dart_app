705b3f00c5d4814daea845fb9151d9aedaee6cf6
andropyro28 <menandroeugenio1028@gmail.com>
2022-07-31 02:24:28 -0400

    OOP

--- bin/first_dart_app.dart
+++ bin/first_dart_app.dart
@@ -1,22 +1,63 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
+// ignore_for_file: public_member_api_docs, sort_constructors_first, must_call_super
 
+import 'dart:convert';
+import 'dart:html';
+
+import 'package:meta/meta.dart';
 
 void main(List<String> arguments) {
- final user1 = User(firstname: "andro", lastname: "eugenio");
+  final admin =
+      Admin(firstname: 'john', lastname: 'doe', specialAdminField: 1.5);
 
-print(user1.fullName);
+  final user = admin as User;
 }
 
-class User {
-  final String firstname;
-  final String lastname;
-  
-  User({
-    required this.firstname,
-    required this.lastname,
-  });
+abstract class User extends Object {
+  // abstract class can have a property or method without a body but you need to implement it to its child class when you make it as a parent class
+  final String _firstname;
+  final String _lastname;
 
-  String getFullName() => "${firstname} ${lastname}";
-  
-  String get fullName => "${firstname} ${lastname}";
+  User(this._firstname, this._lastname);
+
+  factory User.admin() {
+    return Admin(specialAdminField: 123, firstname: 'john', lastname: 'doe');
+  }
+
+  // Admin returnNewAdmin() {
+  //   return Admin(specialAdminField: 123, firstname: 'john', lastname: 'doe');
+  // }
+
+  String get fullname => '${_firstname} ${_lastname}';
+
+  @mustCallSuper
+  void signOut() {
+    print('Signing Out');
+  }
+
+  void myMethod();
+  int get myProperty;
+}
+
+class Admin extends User {
+  final double specialAdminField;
+  Admin(
+      {required String firstname,
+      required String lastname,
+      required this.specialAdminField})
+      : super(firstname, lastname);
+
+  @override
+  String get fullname => 'admin: ${super.fullname}';
+
+  @override
+  void signOut() {
+    print('admin sign out steps');
+    super.signOut();
+  }
+
+  void myMethod() {
+    print('mymethod with implementation');
+  }
+
+  int get myProperty => 15;
 }
--- test/first_dart_app_test.dart
+++ test/first_dart_app_test.dart
@@ -1,6 +1,4 @@
 import 'package:first_dart_app/first_dart_app.dart';
 import 'package:test/test.dart';
 
-void main() {
-  
-}
+void main() {}
